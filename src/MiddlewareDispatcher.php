f9f1e35d0cc4e0d88bbc59799a935c5982c0b656
Julien Duseyau <julien.duseyau@gmail.com>
2025-10-27 15:27:24 +0100

    Refactor MiddlewareDispatcher to use readonly properties and improve middleware dispatching logic

--- src/MiddlewareDispatcher.php
+++ src/MiddlewareDispatcher.php
@@ -13,49 +13,71 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Log\LoggerInterface;
 
-final class MiddlewareDispatcher implements RequestHandlerInterface
+final readonly class MiddlewareDispatcher implements RequestHandlerInterface
 {
-    private int $index = 0;
-
     /**
      * @param list<class-string<MiddlewareInterface>|MiddlewareInterface> $middleware
      */
     public function __construct(
-        private readonly array $middleware,
-        private readonly MiddlewareResolverInterface $resolver,
-        private readonly ?RequestHandlerInterface $finalHandler,
-        private readonly LoggerInterface $logger,
-        private readonly int $verbosity = MiddlewareVerbosity::NORMAL,
+        private array                       $middleware,
+        private MiddlewareResolverInterface $resolver,
+        private ?RequestHandlerInterface    $finalHandler,
+        private LoggerInterface             $logger,
+        private int                         $verbosity = MiddlewareVerbosity::NORMAL,
     ) {
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        if (!isset($this->middleware[$this->index])) {
+        return $this->dispatch(0, $request);
+    }
+
+    /**
+     * @internal Builds the middleware chain recursively without mutable state.
+     *
+     * @throws EmptyMiddlewareChainException
+     */
+    public function dispatch(int $index, ServerRequestInterface $request): ResponseInterface
+    {
+        if (!isset($this->middleware[$index])) {
             if ($this->finalHandler !== null) {
                 if ($this->verbosity >= MiddlewareVerbosity::DEBUG) {
                     $this->logger->info('Final handler invoked');
                 }
+
                 return $this->finalHandler->handle($request);
             }
 
             throw new EmptyMiddlewareChainException();
         }
 
-        $entry = $this->middleware[$this->index++];
+        $entry = $this->middleware[$index];
 
         if (is_string($entry)) {
             $middleware = $this->resolver->resolve($entry);
         } elseif ($entry instanceof MiddlewareInterface) {
             $middleware = $entry;
         } else {
-            throw MiddlewareDispatcherException::invalidEntry($this->index - 1, $entry);
+            throw MiddlewareDispatcherException::invalidEntry($index, $entry);
         }
 
         if ($this->verbosity >= MiddlewareVerbosity::NORMAL) {
             $this->logger->info('Executing middleware', ['middleware' => $middleware]);
         }
 
-        return $middleware->process($request, $this);
+        $nextHandler = new class($this, $index + 1) implements RequestHandlerInterface {
+            public function __construct(
+                private MiddlewareDispatcher $dispatcher,
+                private int $nextIndex
+            ) {
+            }
+
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                return $this->dispatcher->dispatch($this->nextIndex, $request);
+            }
+        };
+
+        return $middleware->process($request, $nextHandler);
     }
 }
