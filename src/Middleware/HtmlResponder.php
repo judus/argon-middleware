b069b63b71cff3c225278dece07355e32fca1f40
Julien Duseyau <julien.duseyau@gmail.com>
2025-10-29 12:14:39 +0100

    Update namespace for HtmlableInterface to improve code organization

--- src/Middleware/HtmlResponder.php
+++ src/Middleware/HtmlResponder.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Maduser\Argon\Middleware\Middleware;
 
-use Maduser\Argon\Middleware\Contracts\Middleware\HtmlableInterface;
+use Maduser\Argon\Support\Contracts\HtmlableInterface;
 use Maduser\Argon\Middleware\Contracts\Middleware\HtmlResponderInterface;
 use Maduser\Argon\Middleware\Contracts\ResultContextInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
